53612f8894d3c9e5a71f10fe2842d3eba98e42f2
openssd <openssd@enc.hanyang.ac.kr>
2017-01-09 13:29:19 +0900

    Fix some strings

--- Source/Firmware/GreedyFTL-2.7.0/nvme/nvme_identify.h
+++ Source/Firmware/GreedyFTL-2.7.0/nvme/nvme_identify.h
@@ -52,7 +52,7 @@
 #define PCI_VENDOR_ID				0x1EDC
 #define PCI_SUBSYSTEM_VENDOR_ID		0x1EDC
 #define SERIAL_NUMBER				"SSDD515T"
-#define MODEL_NUMBER				"ENC_SSD_TIGER3"
+#define MODEL_NUMBER				"Cosmos+ OpenSSD"
 #define FIRMWARE_REVISION			"TYPE0005"
 
 void identify_controller(unsigned int pBuffer);
